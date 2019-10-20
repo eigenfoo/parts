5c2aba070bca1bbc630c36ebe50c08d333677a27
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-10-20 09:08:30 -0400

    MAINT: use magittr pipes to tidy data

--- src/tidy_data.R
+++ src/tidy_data.R
@@ -5,13 +5,14 @@ library(reshape2)
 options(readr.default_locale=readr::locale(tz="America/New_York"))
 
 process_csv <- function(name) {
-  df <- read_csv(sprintf("data/raw/%s.csv", name), col_names = TRUE)
-  df <- melt(df, id.vars = "week.number")
-  df <- rename(df, day = variable, count = value)
-  df <- arrange(df, week.number, day)
-  write_csv(df, sprintf("data/tidy/%s.csv", name))
+  df <-
+    read_csv(sprintf("data/raw/%s.csv", name), col_names = TRUE) %>%
+    melt(id.vars = "week.number") %>%
+    rename(day = variable, count = value) %>%
+    arrange(week.number, day) %>%
+    write_csv(sprintf("data/tidy/%s.csv", name))
 }
-  
+
 for (name in c("analysis_ii", "algebra")) {
   process_csv(name)
 }
