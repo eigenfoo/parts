9f6e64642eff8582279d37ac23bff889189cf03d
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-10-20 00:19:20 -0400

    BLD: tidied data and stored in correct dirs

---
+++ src/tidy_data.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(reshape2)
+
+# From https://stackoverflow.com/a/42638713
+options(readr.default_locale=readr::locale(tz="America/New_York"))
+
+process_csv <- function(name) {
+  df <- read_csv(sprintf("data/raw/%s.csv", name), col_names = TRUE)
+  df <- melt(df, id.vars = "week.number")
+  df <- rename(df, day = variable, count = value)
+  write_csv(df, sprintf("data/tidy/%s.csv", name))
+}
+  
+for (name in c("analysis_ii", "algebra")) {
+  process_csv(name)
+}
