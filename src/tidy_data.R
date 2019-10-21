e6d2ec1e557dbd898604fbbad955accd986420d7
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-10-20 21:53:12 -0400

    MAINT: concatenate tidy data

--- src/tidy_data.R
+++ src/tidy_data.R
@@ -2,17 +2,44 @@ library(tidyverse)
 library(reshape2)
 
 # From https://stackoverflow.com/a/42638713
-options(readr.default_locale=readr::locale(tz="America/New_York"))
+options(readr.default_locale = readr::locale(tz = "America/New_York"))
 
-process_csv <- function(name) {
-  df <-
-    read_csv(sprintf("data/raw/%s.csv", name), col_names = TRUE) %>%
-    melt(id.vars = "week.number") %>%
-    rename(day = variable, count = value) %>%
-    arrange(week.number, day) %>%
-    write_csv(sprintf("data/tidy/%s.csv", name))
-}
+# Analysis II
+analysis <-
+  read_csv(
+    "data/raw/analysis_ii.csv",
+    col_names = TRUE,
+    col_types = cols(
+      week.number = col_double(),
+      mon = col_double(),
+      thur = col_double(),
+      fri = col_double()
+    )
+  ) %>%
+  melt(id.vars = "week.number") %>%
+  rename(day = variable, parts = value) %>%
+  filter(week.number != 9) %>%  # Remove spring break
+  add_column(class = "analysis.ii", .before = 1) %>%
+  arrange(week.number, day) %>%
+  slice(1:(n() - 2))  # Semester ended on a Monday
 
-for (name in c("analysis_ii", "algebra")) {
-  process_csv(name)
-}
+# Algebra
+algebra <-
+  read_csv(
+    "data/raw/algebra.csv",
+    col_names = TRUE,
+    col_types = cols(
+      week.number = col_double(),
+      tue = col_double(),
+      fri = col_double()
+    )
+  ) %>%
+  melt(id.vars = "week.number") %>%
+  rename(day = variable, parts = value) %>%
+  filter(week.number != 4, week.number != 9) %>%  # Remove the flood and spring break
+  add_column(class = "algebra", .before = 1) %>%
+  arrange(week.number, day) %>%
+  slice(1:(n() - 1))  # Semester ended on a Tuesday
+
+bind_rows(analysis, algebra) %>%
+  write_csv("data/tidy/parts.csv")
